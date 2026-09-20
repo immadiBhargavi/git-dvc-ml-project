c315268b37c425a328d996f850f5087ef7142807
Bhargavi <231fa04990immadi@gmail.com>
2026-09-20 21:18:18 +0530

    Add Iris dataset with DVC

---
+++ src/download_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+
+df = pd.DataFrame(
+    iris.data,
+    columns=iris.feature_names
+)
+
+df["target"] = iris.target
+
+df.to_csv("data/iris.csv", index=False)
+
+print("Dataset saved successfully.")
+print(df.head())
