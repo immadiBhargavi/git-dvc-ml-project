2f1bbdeb40824f08e9509d1bf8ebfbfcbbc64cd1
Bhargavi <231fa04990immadi@gmail.com>
2026-09-20 22:03:24 +0530

    Update training parameters for model version 2

--- src/train.py
+++ src/train.py
@@ -20,13 +20,11 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
-# Create Random Forest model
 model = RandomForestClassifier(
-    n_estimators=100,
+    n_estimators=200,
+    max_depth=5,
     random_state=42
-)
-
-# Train model
+)# Train model
 model.fit(X_train, y_train)
 
 # Evaluate model
